571e21bc45434bc678a4b7f09af48bed8c753d05
devegoo <57900186+devegoo@users.noreply.github.com>
2022-08-10 18:30:49 +0200

    Update PowerDynamics-PDC-10-scripts.js

--- PowerDynamics-PDC-10-scripts.js
+++ PowerDynamics-PDC-10-scripts.js
@@ -8,16 +8,16 @@ PowerDynamics_pdc_10.init = function (id, debugging) {
 
 PowerDynamics_pdc_10.shutdown = function() {
    for (var i = 12; i <= 81; i++) {
-        midi.sendShortMsg(0x80, i, 0x00);
+        midi.sendShortMsg(0x90, i, 0x00);
     }
 }
 
 PowerDynamics_pdc_10.wheelTouch = function (channel, control, value, status, group) {
     var deckNumber = script.deckFromGroup(group);
-    if ((status & 0x7F) === 0x90) {
-        var alpha = 1.0/8;
-        var beta = alpha/32;
-        engine.scratchEnable(deckNumber, 128, 33+1/3, alpha, beta);
+    if ((status & 0x7F) === 0x48) {
+        var alpha = 2.0/512;
+        var beta = alpha/512;
+        engine.scratchEnable(deckNumber, 1024, 33+1/3, alpha, beta);
     } else {
         engine.scratchDisable(deckNumber);
     }
